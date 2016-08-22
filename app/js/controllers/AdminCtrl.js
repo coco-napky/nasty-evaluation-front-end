5baf63385fed67a658fc9db795a4ac2e218a66e6
wawila <DavidChavarria@Davids-MacBook-Pro.local>
2016-08-22 14:45:41 -0600

    Admin Add Maestro

--- app/js/controllers/AdminCtrl.js
+++ app/js/controllers/AdminCtrl.js
@@ -1,10 +1,17 @@
-function AdminCtrl(UsersService) {
+function AdminCtrl(UsersService, $state, RolesService) {
   'ngInject'
 
   // ViewModel
   const vm = this;
 
   vm.users = [];
+  vm.nombre = '';
+  
+  
+  let role = window.localStorage['role'];
+    if(!role || role != 1 )
+      $state.go('Home');
+
 
   vm.getUsers = () => UsersService.getUsers(
         data => {
@@ -27,11 +34,35 @@ function AdminCtrl(UsersService) {
     console.log(userId);
   };
 
-vm.getUsers();
+  vm.addRoleToUser = (usuarioId, rolId) => RolesService.addRoleToUser
+  (
+      usuarioId,
+      rolId,      
+      response => {
+        addRoleToUser(response.id,3);
+        console.log(response)
+      },
+      error    => console.log(error)
 
+  );
+
+  vm.postUser = (nombre) => UsersService.postUser(
+      {
+        id: 0,
+        nombre: nombre
+      },
+      () => vm.getUsers(),
+      error    => console.log(error)
+  );
+
+  vm.handlePostUser = () => {
+    if(vm.nombre.length > 0)
+      vm.postUser(vm.nombre);
+  };
+
+
+  vm.getUsers();
 }
-
-
   export default {
     name: 'AdminCtrl',
     fn: AdminCtrl
